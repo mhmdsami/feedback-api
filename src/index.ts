41abf4e991d5fc0eca9ad1c3f38771ce09b646b8
Mohamed Sami <srmf2025@gmail.com>
2024-04-15 17:19:20 +0530

    feat: error handling

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,46 @@
 import { Elysia } from "elysia";
-import { api } from "./controllers";
-import { PORT } from "../utils/config";
+import { api } from "@/controllers";
+import { PORT } from "@/utils/config";
 
 export const app = new Elysia()
-  .get("/", () => "Hello World!")
+  .get("/", () => {
+    return {
+      message: "Welcome to the Feedback API!",
+    };
+  })
   .get("/healthcheck", () => {
     return {
       status: "ok",
       uptime: process.uptime(),
     };
+  })
+  .onError(({ code, error }) => {
+    switch (code) {
+      case "NOT_FOUND": {
+        return Response.json(
+          {
+            message: "The requested resource was not found",
+          },
+          { status: 404 },
+        );
+      }
+      case "VALIDATION": {
+        return Response.json(
+          {
+            message: "Validation error",
+            errors: error.validator.Errors(error.value).First().schema.error
+          },
+          { status: 400 },
+        );
+      }
+      default:
+        Response.json(
+          {
+            message: "Internal Server Error",
+          },
+          { status: 500 },
+        );
+    }
   })
   .use(api)
   .listen(PORT);
