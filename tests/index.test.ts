93e963ebd5a77d037d12383b5a6d997b8f01bd75
Mohamed Sami <srmf2025@gmail.com>
2024-04-16 08:20:46 +0530

    test: add index route tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,12 +2,38 @@
 import { app } from "@/index";
 import { describe, expect, it } from "bun:test";
 
-describe("App", () => {
+describe("API", () => {
   it("return a response", async () => {
     const response = await app
       .handle(new Request("http://localhost/"))
-      .then((res) => res.text());
+    expect(response.status).toBe(200);
 
-    expect(response).toBe("Hello World!");
+    const body = await response.json();
+    expect(body).toEqual({
+      message: "Welcome to the Feedback API!",
+    });
+  });
+
+  it("health is ok", async () => {
+    const response = await app
+      .handle(new Request("http://localhost/healthcheck"))
+    expect(response.status).toBe(200);
+
+    const body = await response.json();
+    expect(body).toEqual({
+      status: "ok",
+      uptime: expect.any(Number),
+    });
+  });
+
+  it("return 404", async () => {
+    const response = await app
+      .handle(new Request("http://localhost/unknown"))
+    expect(response.status).toBe(404);
+
+    const body = await response.json();
+    expect(body).toEqual({
+      message: "The requested resource was not found",
+    });
   });
 });
